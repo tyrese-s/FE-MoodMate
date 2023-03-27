bb6fd514062e39148871b186451818dc48e4ab73
CharlieH94 <94218945+CharlieH94@users.noreply.github.com>
2023-03-27 15:15:10 +0100

    login form ongoing

--- moodmateBlank/components/Login.tsx
+++ moodmateBlank/components/Login.tsx
@@ -1,13 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import Constants from 'expo-constants';
+import { useForm } from 'react-hook-form';
 
 export default function Login () {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("username"));
+  console.log(watch("password"));
+
+
     return (
+      <KeyboardAwareScrollView>
         <View style={styles.container}>
-          <Text>Open up App.tsx to start working on your app!</Text>
-          <StatusBar style="auto" />
+          <form style={styles.form}>
+            <label htmlFor='username'>Username</label>
+            <input placeholder='username' {...register("username", { required: true })} id='username'/>
+            {errors.username && <span>This field is required</span>}
+            <label htmlFor='password'>Password</label>
+            <input placeholder='password'{...register("password", { required: true })} id='password'/>
+            {errors.password && <span>This field is required</span>}
+            <Button title="submit" onPress={handleSubmit(onSubmit)}  />
+          </form>
         </View> 
+      </KeyboardAwareScrollView>
     )
 }
 
@@ -18,4 +36,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  form: {
+    width: '50%',
+  }
 });
