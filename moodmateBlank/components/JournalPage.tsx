1fa4fed794b09c0c3d63c6add62ca588ab8c5fe5
Jake Samuels <samuels242@googlemail.com>
2023-04-02 09:29:09 +0100

    Modify names of JournalPage fields

--- moodmateBlank/components/JournalPage.tsx
+++ moodmateBlank/components/JournalPage.tsx
@@ -5,25 +5,24 @@ import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view
 import { SelectList } from "react-native-dropdown-select-list";
 import React from "react";
 
+export default function JournalPage() {
+  const [selected, setSelected] = React.useState("");
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-export default function JournalPage () {
-    const [selected, setSelected] = React.useState("");
-    const {
-        control,
-        handleSubmit,
-        formState: { errors },
-      } = useForm();
+  const onSubmit = (data: any): void => {
+    console.log({ ...data, selected });
+  };
 
-      const onSubmit = (data: any): void => {
-        console.log({data, selected});
-      }
+  interface Props {
+    name: string;
+    control: any;
+  }
 
-      interface Props {
-        name: string;
-        control: any;
-      }
-
-const Input = (props: Props) => {
+  const Input = (props: Props) => {
     const { name, control } = props;
     const { field } = useController({
       control,
@@ -41,48 +40,49 @@ const Input = (props: Props) => {
   };
 
   const howImFeeling = [
-    {key:'10', value:'10'},
-    {key:'9', value:'9'},
-    {key:'8', value:'8'},
-    {key:'7', value:'7'},
-    {key:'6', value:'6'},
-    {key:'5', value:'5'},
-    {key:'4', value:'4'},
-    {key:'3', value:'3'},
-    {key:'2', value:'2'},
-    {key:'1', value:'1'},
-  ]
-    return (
-        <SafeAreaView style={styles.container}>
-            <KeyboardAwareScrollView>
-            <Text style={styles.title}>Mood:</Text>
-            <Input name="Mood" control={control} />
-            <Text style={styles.title}>Overview</Text>
-            <Input name="Overview" control={control}/>
-            <Text style={styles.title}>Food & Drink</Text>
-            <Input name="Food&Drink" control={control}  />
-            <Text style={styles.title}>Exercise</Text>
-            <Input name="Exercise" control={control} />
-            <Text style={styles.title}>How are you feeling?</Text>
-            <SelectList 
-                setSelected={(value: string) => setSelected(value)} 
-                data={howImFeeling} 
-                save="value"/>
-            <Button title="Submit Journal entry" onPress={handleSubmit(onSubmit)} />
-            </KeyboardAwareScrollView>
-        </SafeAreaView>
-    )
+    { key: "10", value: "10" },
+    { key: "9", value: "9" },
+    { key: "8", value: "8" },
+    { key: "7", value: "7" },
+    { key: "6", value: "6" },
+    { key: "5", value: "5" },
+    { key: "4", value: "4" },
+    { key: "3", value: "3" },
+    { key: "2", value: "2" },
+    { key: "1", value: "1" },
+  ];
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAwareScrollView>
+        <Text style={styles.title}>Mood:</Text>
+        <Input name="mood" control={control} />
+        <Text style={styles.title}>Overview</Text>
+        <Input name="overview" control={control} />
+        <Text style={styles.title}>Food & Drink</Text>
+        <Input name="diet" control={control} />
+        <Text style={styles.title}>Exercise</Text>
+        <Input name="exercise" control={control} />
+        <Text style={styles.title}>How are you feeling?</Text>
+        <SelectList
+          setSelected={(value: string) => setSelected(value)}
+          data={howImFeeling}
+          save="value"
+        />
+        <Button title="Submit Journal entry" onPress={handleSubmit(onSubmit)} />
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EED2E7',
-    alignItems: 'center'
+    backgroundColor: "#EED2E7",
+    alignItems: "center",
   },
   title: {
     fontSize: 24,
-    textAlign: 'center',
+    textAlign: "center",
     paddingTop: 16,
     paddingBottom: 4,
   },
@@ -91,6 +91,6 @@ const styles = StyleSheet.create({
     height: 40,
     borderWidth: 1,
     width: 350,
-    padding: 10
-  }
-})
+    padding: 10,
+  },
+});
