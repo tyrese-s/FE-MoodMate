e622662b076bc2d58d1339db1f5421c2b306bc57
Matthew Jenner <matthewjenner@hotmail.com>
2023-04-03 22:48:32 +0100

    calendar background

--- moodmateBlank/assets/Images.js
+++ moodmateBlank/assets/Images.js
@@ -3,5 +3,7 @@ export const images = {
     background: { uri: 'https://images.unsplash.com/photo-1572615318109-4b6595947181?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' },
     meditate: {
         uri: "https://images.pexels.com/photos/1032653/pexels-photo-1032653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      }
+      },
+      calendar: { uri: 'https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?w=2000'}
+
 }
