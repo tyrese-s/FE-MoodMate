c66416d5f585f2c02aaee0d3e879bc3a4b600232
CharlieH94 <94218945+CharlieH94@users.noreply.github.com>
2023-04-04 07:59:03 +0100

    moodpage styling

--- moodmateBlank/components/MoodPage.tsx
+++ moodmateBlank/components/MoodPage.tsx
@@ -1,94 +1,165 @@
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { View, Text, Pressable, StyleSheet, TouchableOpacity, FlatList } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import {
+  View,
+  Text,
+  Pressable,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  ImageBackground,
+  Dimensions,
+} from "react-native";
 import { useEffect, useState } from "react";
 import { getSingleEmotion } from "../utils/api.js";
 
 interface Emotion {
-        emotion: string;
-        mainText: string[];
-        causes: string[];
-        commonThoughts: string;
-        reassurance: string;
-        techniques: string[];
-  }
+  emotion: string;
+  mainText: string[];
+  causes: string[];
+  commonThoughts: string;
+  reassurance: string;
+  techniques: string[];
+}
 
-export default function MoodPage ( {route}: {route: any} ) {
-    const { emotionType } = route.params
-    const [singleEmotion, setSingleEmotion] = useState<Emotion | null>(null);
+export default function MoodPage({ route }: { route: any }) {
+  const { emotionType } = route.params;
+  const [singleEmotion, setSingleEmotion] = useState<Emotion | null>(null);
 
+  useEffect(() => {
+    getSingleEmotion(emotionType).then((emotionFromApi) => {
+      setSingleEmotion(emotionFromApi[0]);
+    });
+  }, [emotionType]);
 
-    useEffect(() => {
-      getSingleEmotion(emotionType)
-      .then((emotionFromApi) => {
-          setSingleEmotion(emotionFromApi[0])
-      })
-  }, [emotionType])
+  const capitaliser = (word?: string): string | undefined => {
+    if (word) return word[0].toUpperCase() + word.slice(1).toLowerCase();
+  };
 
-    const capitaliser = (word?: string): string | undefined => {
-        if(word) return word[0].toUpperCase() + word.slice(1).toLowerCase();
-    }
-    
-     return (
-        <KeyboardAwareScrollView style={styles.container}>
-            <View>
-                <Text style={styles.emotionTitle}>{capitaliser(singleEmotion?.emotion)}</Text>
-            </View>
-            <View>
-                {singleEmotion?.mainText.map(sentence => {
-                    return <Text style={styles.sentence} key={sentence}>{sentence}</Text>
-                })} 
-            </View>
-             <View>
-                 <Text style={styles.title}>Possible Causes & Triggers</Text>
-                 {singleEmotion?.causes.map(cause => {
-                     return <Text style={styles.information} key={cause}>{cause}</Text>
-                 })}
-            </View>
-             <View>
-                <Text style={styles.title}>Common Thoughts</Text>
-                <Text style={styles.information}>{singleEmotion?.commonThoughts}</Text>
-            </View>
-             <View>
-                <Text style={styles.title}>Reassurance</Text>
-                <Text style={styles.information}>{singleEmotion?.reassurance}</Text>
-            </View>
-             <View style={{marginBottom: 16}}>
-                 <Text style={styles.title}>Coping Techniques</Text>
-                 {singleEmotion?.techniques.map(technique => {
-                     return <Text style={styles.information} key={technique}>{technique}</Text>
-                 })}
-            </View>
-        </KeyboardAwareScrollView>
-    ) 
+  return (
+    <View style={styles.container}>
+      <KeyboardAwareScrollView style={styles.scrollview}>
+        <View>
+          <Text style={styles.emotionTitle}>
+            {capitaliser(singleEmotion?.emotion)}
+          </Text>
+        </View>
+        <View>
+          <View style={styles.textBody}>
+            {singleEmotion?.mainText.map((sentence) => {
+              return (
+                <Text style={styles.sentence} key={sentence}>
+                  {sentence}
+                </Text>
+              );
+            })}
+          </View>
+        </View>
+        <View>
+          <Text style={styles.title}>Possible Causes & Triggers</Text>
+          <View style={styles.textBody}>
+            {singleEmotion?.causes.map((cause) => {
+              return (
+                <Text style={styles.information} key={cause}>
+                  {cause}
+                </Text>
+              );
+            })}
+          </View>
+        </View>
+        <View>
+          <Text style={styles.title}>Common Thoughts</Text>
+          <View style={styles.textBody}>
+            <Text style={styles.information}>
+              {singleEmotion?.commonThoughts}
+            </Text>
+          </View>
+        </View>
+        <View>
+          <Text style={styles.title}>Reassurance</Text>
+          <View style={styles.textBody}>
+            <Text style={styles.information}>{singleEmotion?.reassurance}</Text>
+          </View>
+        </View>
+        <View style={{ marginBottom: 16 }}>
+          <Text style={styles.title}>Coping Techniques</Text>
+          <View style={styles.textBody}>
+            {singleEmotion?.techniques.map((technique) => {
+              return (
+                <Text style={styles.information} key={technique}>
+                  {technique}
+                </Text>
+              );
+            })}
+          </View>
+        </View>
+      </KeyboardAwareScrollView>
+      <ImageBackground
+        style={[styles.fixed, { zIndex: -1 }]}
+        source={{
+          uri: "https://images.pexels.com/photos/4175070/pexels-photo-4175070.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        }}
+        imageStyle={{ opacity: 0.5, tintColor: "green" }}
+      />
+      <ImageBackground
+        style={[styles.fixed, { zIndex: -1 }]}
+        source={{
+          uri: "https://images.pexels.com/photos/4175070/pexels-photo-4175070.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        }}
+        imageStyle={{ opacity: 0.6 }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    emotionTitle: {
-        textAlign: 'center',
-        fontSize: 30,
-        marginVertical: 16,
-        fontWeight: 'bold',
-    },
-    information: {
-        fontSize: 16,
-        textAlign: 'center',
-        marginHorizontal: 24,
-        marginVertical: 4,
-    },
-    sentence: {
-        paddingHorizontal: 24,
-        paddingBottom: 8,
-        fontSize: 14,
-    },  
-    title: {
-        fontSize: 24,
-        textAlign: 'center',
-        paddingTop: 16,
-        paddingBottom: 8,
-    }
-
-})
+  container: {
+    flex: 1,
+    // backgroundColor: '#fff',
+  },
+  emotionTitle: {
+    textAlign: "center",
+    fontSize: 30,
+    marginVertical: 16,
+    fontWeight: "bold",
+    textShadowOffset: { width: 0, height: 0 },
+    textShadowRadius: 2,
+    textShadowColor: "white",
+  },
+  information: {
+    fontSize: 16,
+    textAlign: "center",
+    marginHorizontal: 24,
+    marginVertical: 4,
+  },
+  sentence: {
+    paddingHorizontal: 24,
+    paddingBottom: 8,
+    fontSize: 14,
+  },
+  textBody: {
+    backgroundColor: "rgba(255,255,255, 0.5)",
+    borderRadius: 30,
+    marginHorizontal: 12,
+    paddingVertical: 12,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: "center",
+    paddingTop: 16,
+    paddingBottom: 8,
+    fontWeight: "500",
+    textShadowOffset: { width: 0, height: 0 },
+    textShadowRadius: 2,
+    textShadowColor: "white",
+  },
+  fixed: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  scrollview: {
+    backgroundColor: "transparent",
+  },
+});
