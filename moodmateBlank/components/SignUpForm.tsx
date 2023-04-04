1fba5ca04607df8616c5bbdc548a40d335583189
CharlieH94 <94218945+CharlieH94@users.noreply.github.com>
2023-04-04 10:21:25 +0100

    signup padding

--- moodmateBlank/components/SignUpForm.tsx
+++ moodmateBlank/components/SignUpForm.tsx
@@ -99,6 +99,7 @@ const styles = StyleSheet.create({
     marginTop: 4,
     marginBottom: 16,
     padding: 8,
+    paddingHorizontal: 16,
     borderRadius: 30,
     borderWidth: 1,
     borderColor: 'silver'
