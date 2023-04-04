513ae57704318f1854d728865c808dad9a6e3239
Jake Samuels <samuels242@googlemail.com>
2023-04-04 23:24:20 +0100

    Add resetPassword

--- moodmateBlank/utils/api.js
+++ moodmateBlank/utils/api.js
@@ -1,187 +1,154 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getJournalEntries =
-  exports.saveJournalEntry =
-  exports.deleteQuote =
-  exports.getAllQuotes =
-  exports.saveQuote =
-  exports.updateUser =
-  exports.loginUser =
-  exports.signupUser =
-  exports.getGoogleVisionURL =
-  exports.getSingleEmotion =
-  exports.getEmotions =
-  exports.getRandomZenQuote =
-    void 0;
-var axios_1 = require("axios");
+import axios from "axios";
+
 // BASE URLS
-var moodmateApi = axios_1.default.create({
+const moodmateApi = axios.create({
   baseURL: "https://moodmate-api.onrender.com/api/v1",
 });
-var zenQuotesAPI = axios_1.default.create({
+
+const zenQuotesAPI = axios.create({
   // baseURL: "https://zenquotes.io/api/random/"
   baseURL: "https://zenquotes.io/api/today",
   // baseURL: "https://zenquotes.io/api/quotes"
 });
-var emotionsApi = axios_1.default.create({
+
+const emotionsApi = axios.create({
   baseURL: "https://emotions-api-w230.onrender.com/api",
 });
+
 // ZEN QUOTES
-var getRandomZenQuote = function (input) {
-  return zenQuotesAPI.get(input).then(function (response) {
-    var responseBody = response.data[0];
+export const getRandomZenQuote = () => {
+  return zenQuotesAPI.get().then((response) => {
+    const responseBody = response.data[0];
     return { quote: responseBody.q, author: responseBody.a };
   });
 };
-exports.getRandomZenQuote = getRandomZenQuote;
+
 // EMOTIONS API
-var getEmotions = function () {
-  return emotionsApi.get("/v1/emotions").then(function (_a) {
-    var data = _a.data;
+export const getEmotions = () => {
+  return emotionsApi.get("/v1/emotions").then(({ data }) => {
     return data.emotions;
   });
 };
-exports.getEmotions = getEmotions;
-var getSingleEmotion = function (emotion) {
-  return emotionsApi.get("/v1/emotions/".concat(emotion)).then(function (_a) {
-    var data = _a.data;
+
+export const getSingleEmotion = (emotion) => {
+  return emotionsApi.get(`/v1/emotions/${emotion}`).then(({ data }) => {
     return data.emotion;
   });
 };
-exports.getSingleEmotion = getSingleEmotion;
+
 // GOOGLE VISION API
-var getGoogleVisionURL = function () {
-  return moodmateApi.get("/api-url").then(function (response) {
-    var url = response.data.data.apiURL.substring(
+export const getGoogleVisionURL = () => {
+  return moodmateApi.get("/api-url").then((response) => {
+    const url = response.data.data.apiURL.substring(
       response.data.data.apiURL.indexOf("=") + 1
     );
     return url;
   });
 };
-exports.getGoogleVisionURL = getGoogleVisionURL;
+
 // BACKEND API
 // AUTH
-var signupUser = function (data) {
-  return moodmateApi.post("/users/signup", data).then(function (response) {
+export const signupUser = (data) => {
+  return moodmateApi.post("/users/signup", data).then((response) => {
     return response.data;
   });
 };
-exports.signupUser = signupUser;
-var loginUser = function (data) {
+
+export const loginUser = (data) => {
   return moodmateApi
     .post("/users/login", data)
-    .then(function (response) {
+    .then((response) => {
       return response.data;
     })
-    .catch(function (error) {
-      return console.error(error);
-    });
+    .catch((error) => console.error(error));
 };
-exports.loginUser = loginUser;
-var updateUser = function (data, userToken) {
+
+export const updateUser = (data, userToken) => {
   return moodmateApi
     .patch("/users/updateMe", data, {
       headers: {
-        Authorization: "Bearer ".concat(userToken),
+        Authorization: `Bearer ${userToken}`,
       },
     })
-    .then(function (response) {
+    .then((response) => {
       console.log(response);
       // return response.data;
     })
-    .catch(function (error) {
-      return console.error(error);
-    });
+    .catch((error) => console.error(error));
 };
-exports.updateUser = updateUser;
 
-var resetPassword = function (data) {
+export const resetPassword = (data) => {
   return moodmateApi
     .patch("/users/resetPassword", data)
-    .then(function (response) {
+    .then((response) => {
       return response.data.data.user;
     })
-    .catch(function (error) {
-      return console.error(error);
-    });
+    .catch((error) => console.error(error));
 };
 
-exports.resetPassword = resetPassword;
-
 // QUOTES
-var saveQuote = function (quoteData, userToken) {
+export const saveQuote = (quoteData, userToken) => {
   return moodmateApi
     .post("/quotes/addQuote", quoteData, {
       headers: {
-        Authorization: "Bearer ".concat(userToken),
+        Authorization: `Bearer ${userToken}`,
       },
     })
-    .then(function (response) {
+    .then((response) => {
       return response.data;
     })
-    .catch(function (error) {
-      return console.error(error);
-    });
+    .catch((error) => console.error(error));
 };
-exports.saveQuote = saveQuote;
-var getAllQuotes = function (userToken) {
+
+export const getAllQuotes = (userToken) => {
   return moodmateApi
     .get("/quotes", {
       headers: {
-        Authorization: "Bearer ".concat(userToken),
+        Authorization: `Bearer ${userToken}`,
       },
     })
-    .then(function (response) {
+    .then((response) => {
       return response.data;
     })
-    .catch(function (error) {
-      return console.error(error);
-    });
+    .catch((error) => console.error(error));
 };
-exports.getAllQuotes = getAllQuotes;
-var deleteQuote = function (quoteId, userToken) {
+
+export const deleteQuote = (quoteId, userToken) => {
   return moodmateApi
-    .delete("/quotes/".concat(quoteId), {
+    .delete(`/quotes/${quoteId}`, {
       headers: {
-        Authorization: "Bearer ".concat(userToken),
+        Authorization: `Bearer ${userToken}`,
       },
     })
-    .then(function (response) {
+    .then((response) => {
       return response.data;
     })
-    .catch(function (error) {
-      return console.error(error);
-    });
+    .catch((error) => console.error(error));
 };
-exports.deleteQuote = deleteQuote;
+
 // JOURNAL
-var saveJournalEntry = function (journalEntry, userToken) {
+export const saveJournalEntry = (journalEntry, userToken) => {
   return moodmateApi
     .post("/journal/entries", journalEntry, {
       headers: {
-        Authorization: "Bearer ".concat(userToken),
+        Authorization: `Bearer ${userToken}`,
       },
     })
-    .then(function (response) {
+    .then((response) => {
       return response.data;
     })
-    .catch(function (error) {
-      return console.error(error);
-    });
+    .catch((error) => console.error(error));
 };
-exports.saveJournalEntry = saveJournalEntry;
-var getJournalEntries = function (date, userToken) {
+
+export const getJournalEntries = (date, userToken) => {
   return moodmateApi
-    .get("/journal/entries/".concat(date), {
+    .get(`/journal/entries/${date}`, {
       headers: {
-        Authorization: "Bearer ".concat(userToken),
+        Authorization: `Bearer ${userToken}`,
       },
     })
-    .then(function (response) {
+    .then((response) => {
       return response.data.data.entries;
     })
-    .catch(function (error) {
-      return console.error(error);
-    });
+    .catch((error) => console.error(error));
 };
-exports.getJournalEntries = getJournalEntries;
